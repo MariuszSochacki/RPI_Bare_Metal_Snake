d3a582ce701ad88ba26f7cf9f0cc2286f9de33ba
MariuszSochacki <msochacki1@gmail.com>
2019-03-05 11:43:00 +0100

    Clean up bootloader to stop unused cores

--- src/boot.s
+++ src/boot.s
@@ -2,6 +2,12 @@
 
 .globl _start
 _start:
-    mov sp,#0x00010000 // TODO: BSS clear, separate cores
+    mrs     x1, mpidr_el1
+    and     x1, x1, #3
+    cbnz    x1, hang
+    ldr     x1, =_start
+    mov     sp, x1
     bl main
-hang: b hang
+hang:
+    wfe
+    b hang
